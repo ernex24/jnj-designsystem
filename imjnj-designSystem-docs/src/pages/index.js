b3de0702546ed93a4a5b9debb328a56d5b1be89c
Araque, Ernesto [JNJCH NON-J&J] <EAraque1@its.jnj.com>
2024-05-02 10:06:56 +0200

    Test

---
+++ imjnj-designSystem-docs/src/pages/index.js
@@ -0,0 +1,51 @@
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import Button from '@mui/material/Button';
+
+import Heading from '@theme/Heading';
+import styles from './index.module.css';
+
+import '@imjnj/tokens/dist/styles.css'
+
+import jnjTheme from '@imjnj/tokens/dist'
+
+console.log(jnjTheme)
+
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <Heading as="h1" className="hero__title jnj-margin-bottom-16">
+          {siteConfig.title}
+        </Heading>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/intro">
+            Docusaurus Tutorial - 5min ⏱️
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default function Home() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <Layout
+      title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />">
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+      </main>
+    </Layout>
+  );
+}
